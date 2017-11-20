9e0aeacdd571d0d8112af3be8695fbdb2d03e05c
bacancypratik <pratik.panchal@bacancytechnology.com>
2017-11-20 13:25:55 +0530

    Project added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CableTvOperator::Application.config.secret_token = 'da351a7da100234e7b24976dab11a135dc9ddd5bfb8ec3ad1e9ddd5b3943025f1575ca5100f95797d6d5415fd47ebddc69f182c0c947c592a07c9bbbcc473040'
